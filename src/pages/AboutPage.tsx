caec743f75ddb6092abd18d46830671bcad8d69b
william.burgeson <William.Burgeson@googlemail.com>
2025-11-18 10:23:25 +0100

    added about page and wired up routes

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is a simple About page for the demo router.</p>
+      <p>
+        The Home page shows the Vite + React logos and a counter. This page is a
+        lightweight placeholder.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
